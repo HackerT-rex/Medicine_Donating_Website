8ed422adc0d8a022a8541c2644c3b15b52f580d9
HackerT-rex <tamilselvan9092457581@gmail.com>
2024-03-14 19:34:13 +0530

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Redirect to home page
+    window.location.href = 'website.html';
+});
+
+function loginWithGoogle() {
+  // Simulate Google login
+  console.log("Login with Google");
+  // Redirect to home.html after login
+  window.location.href = "website.html";
+}
+
+function loginWithMicrosoft() {
+  // Simulate Microsoft login
+  console.log("Login with Microsoft");
+  // Redirect to home.html after login
+  window.location.href = "website.html";
+}
