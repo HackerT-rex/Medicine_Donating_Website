ed516b964b43d2ecc38c6a46c4d3e4d43145c83f
HackerT-rex <tamilselvan9092457581@gmail.com>
2024-03-28 14:15:14 +0530

    commit

--- buyer.py
+++ buyer.py
@@ -1,26 +1,23 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def order_form():
-    if request.method == 'POST':
-        buyer_name = request.form['buyer_name']
-        buyer_address = request.form['buyer_address']
-        medicine_name = request.form['medicine_name']
-        quantity = request.form['quantity']
+@app.route('/')
+def index():
+    return render_template('your_webpage.html')
 
-        # Do something with the form data, for example, print it
-        print("Buyer Name:", buyer_name)
-        print("Buyer Address:", buyer_address)
-        print("Medicine Name:", medicine_name)
-        print("Quantity:", quantity)
+@app.route('/submit', methods=['POST'])
+def submit():
+    medicine_name = request.form['medicine_name']
+    quantity = request.form['quantity']
+    
+    with open('medicine_donations.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([medicine_name, quantity])
 
-        # Return a response indicating the form was submitted
-        return "Order placed successfully!"
-
-    # Render the HTML template containing the form
-    return render_template('buyer.html')
+    return 'Medicine donation submitted successfully!'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
